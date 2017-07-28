c3a80178e00bd44a5c9d218b3ae9c44faa772cd5
Meiyo40 <loic.demesmay@gmail.com>
2017-07-28 18:01:56 +0200

    Update du programme hello world avec ajout d'un input et d'un test avec conditions

--- premierPython.py
+++ premierPython.py
@@ -3,3 +3,10 @@
 
 
 print ("Hello world!")
+answer = input ("Comment allez vous aujourd\'hui ?")
+if (answer == 'bien'):
+    print("Tant mieux parce que t\'as du boulot devant toi mon gars!")
+elif (answer == 'pas bien'):
+    print("C\'est pas bien grave, suffit de se plonger un peu dans le code et hop on passe a autre chose")
+else:
+    print("Meme si je suis une machine tu pourrais me repondre :(")
